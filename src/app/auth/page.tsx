e6b4efa0dad1d524d3dac3b97917a050e2d4092e
SworZzey <i190907@yandex.ru>
2025-10-11 22:53:51 +0700

    Измененние стилей на странице входа

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -7,12 +7,12 @@ export default function Auth(){
     const router = useRouter();
     const { themeParams } = useTelegram();
     return (
-        <>
-            <h2 className="mt-[30px] bg-none text-[#2C3E50] text-2xl font-bold text-center">Вход в личный кабинет</h2>
-            <div className="min-h-screen flex items-center justify-center w-full bg-gray-50">
+        <div className="bg-[#ECF0F1] min-h-screen">
+            <h2 className="pt-[30px] bg-none text-[#2C3E50] text-2xl font-bold text-center">Вход в личный кабинет</h2>
+            <div className="flex items-center justify-center w-full">
                 <div className="w-full max-w-[100%] mx-4">
                     <form className="bg-transparent">
-                        <div className="mb-6 flex justify-center">
+                        <div className="flex justify-center mt-8">
                             <input
                                 type="text"
                                 className="w-[90%] h-[70px] rounded-[20px] bg-[#34495E] text-white placeholder:text-white placeholder:text-xs placeholder:font-bold"
@@ -20,10 +20,10 @@ export default function Auth(){
                             />
                         </div>
 
-                        <div className="flex flex-col space-y-4 items-center">
+                        <div className="flex flex-col space-y-4 items-center mt-8">
                             <button
                                 type="submit"
-                                className="w-[90%] text-white bg-[#3498DB] rounded-[20px] text-xs font-bold"
+                                className="w-[90%] h-[50px] text-white bg-[#3498DB] rounded-[20px] text-xs font-bold"
                             >
                                 Войти
                             </button>
@@ -31,6 +31,6 @@ export default function Auth(){
                     </form>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
