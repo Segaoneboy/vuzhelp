1ee67c298391cd2b13692c25895e6a65d925d612
SworZzey <i190907@yandex.ru>
2025-10-11 23:02:06 +0700

    Измененние стилей на странице входа

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -9,18 +9,18 @@ export default function Auth(){
     return (
         <div className="bg-[#ECF0F1] min-h-screen">
             <h2 className="pt-[30px] bg-none text-[#2C3E50] text-2xl font-bold text-center">Вход в личный кабинет</h2>
-            <div className="flex items-center justify-center w-full">
+            <div className="flex items-center justify-center w-full h-[70vh]">
                 <div className="w-full max-w-[100%] mx-4">
                     <form className="bg-transparent">
                         <div className="flex justify-center mt-8">
                             <input
                                 type="text"
-                                className="w-[90%] h-[70px] rounded-[20px] bg-[#34495E] text-white placeholder:text-white placeholder:text-xs placeholder:font-bold"
+                                className="w-[90%] h-[70px] rounded-[20px] bg-[#34495E] text-white placeholder:text-white placeholder:text-xs placeholder:font-bold pl-8"
                                 placeholder="Ваш ID"
                             />
                         </div>
 
-                        <div className="flex flex-col space-y-4 items-center mt-8">
+                        <div className="flex flex-col space-y-4 items-center mt-[18px]">
                             <button
                                 type="submit"
                                 className="w-[90%] h-[50px] text-white bg-[#3498DB] rounded-[20px] text-xs font-bold"
