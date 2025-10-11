9721401dcf1c3800f605add84bbdf3ccc6300d08
SworZzey <i190907@yandex.ru>
2025-10-11 22:26:45 +0700

    Добавлена страница входа

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -8,8 +8,28 @@ export default function Auth(){
     const { themeParams } = useTelegram();
     return (
         <>
-            <div style={{background: themeParams.bg_color, color: themeParams.color}}>
-                <button onClick={() => router.push("/")} className='bg-amber-300 p-3'>Перейти на главную</button>
+            <h1 className="text-[#2C3E50] text-2xl font-bold">Вход в личный кабинет</h1>
+            <div className="min-h-screen flex items-center justify-center w-full bg-gray-50">
+                <div className="w-full max-w-[90%] mx-4">
+                    <form className="bg-transparent shadow-lg rounded-lg p-8">
+                        <div className="mb-6 flex justify-center">
+                            <input
+                                type="text"
+                                className="w-full max-w-[90%] h-[71px] px-3 py-2 bg-[#34495E] text-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder-white placeholder:text-xs placeholder:font-bold"
+                                placeholder="Ваш ID"
+                            />
+                        </div>
+
+                        <div className="flex flex-col space-y-4 items-center">
+                            <button
+                                type="submit"
+                                className="w-[90%] h-[50px] bg-[#3498DB] hover:bg-[#2980B9] text-white font-bold py-3 px-4 rounded-md transition duration-200"
+                            >
+                                Войти
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </>
     )
