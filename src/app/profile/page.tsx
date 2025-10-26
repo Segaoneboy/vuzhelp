79af0305d3554b70241af9f9fd5a87dbd3f6521c
Sergey Peressypkin <peresypkinwork@gmail.com>
2025-10-26 19:26:35 +0700

    user test

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -16,23 +16,24 @@ export default function Profile(){
             <h2 className="pt-[30px] bg-none text-[#2C3E50] text-2xl font-bold text-center">Личный кабинет</h2>
             <div className="flex flex-col items-center space-y-4 mt-8">
                 {/*Фото профиля*/}
-                {/*{user ? (*/}
-                {/*    <Image*/}
-                {/*        src={user.photo_url}*/}
-                {/*        alt="user avatar"*/}
-                {/*        className="rounded-full"*/}
-                {/*        width={82}*/}
-                {/*        height={82}*/}
-                {/*    />*/}
-                {/*) : (*/}
-                {/*    <div className="w-[82px] h-[82px] bg-gray-300 rounded-full" />*/}
-                {/*)}*/}
+                {user ? (
+                    <Image
+                        src={user.photo_url}
+                        alt="user avatar"
+                        className="rounded-full"
+                        width={82}
+                        height={82}
+                    />
+                ) : (
+                    <div className="w-[82px] h-[82px] bg-gray-300 rounded-full" />
+                )}
 
                 {/*Фио и балл пользователя*/}
                 <div>
                     <p className="text-[#2C3E50] text-base font-bold">Питер Паркер</p>
                     <p className="text-[#2C3E50] text-base font-bold">Ваш балл за ЕГЭ: <b className="montserrat-text text-[#3498DB] text-base font-bold">256</b></p>
                     <button className = "text-[#2C3E50] text-base font-bold">Пригласить друга</button>
+                    <p>{user}</p>
                 </div>
             </div>
         </div>
