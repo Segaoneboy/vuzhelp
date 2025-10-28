0765cbe50b16460229fcce6adf3a4240a3c603f2
SworZzey <i190907@yandex.ru>
2025-10-28 18:09:21 +0700

    Исправление страницы Регистрации

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,55 +1,47 @@
 'use client';
-export const dynamic = 'force-dynamic';
+import { useRouter } from "next/navigation"
+import {useTelegram} from "@/providers/TelegramProviders";
 
-import { useRouter } from "next/navigation";
-import { useTelegram } from "@/providers/TelegramProviders";
-import Image from 'next/image';
-
-export default function Profile() {
+export default function Auth() {
     const router = useRouter();
-    const { user } = useTelegram();
-
-    // Пока user не загрузился
-    if (!user) {
-        return (
-            <div className="bg-[#ECF0F1] min-h-screen flex items-center justify-center">
-                <p className="text-[#7f8c8d] text-base">Загрузка профиля...</p>
-            </div>
-        );
-    }
-
-    const avatar = user.photo_url || '/user.png';
-
+    const { themeParams } = useTelegram();
     return (
         <div className="bg-[#ECF0F1] min-h-screen">
-            <h2 className="pt-[30px] bg-none text-[#2C3E50] text-2xl font-bold text-center">
-                Личный кабинет
-            </h2>
+            <h2 className="montserrat-text pt-[30px] bg-none text-[#2C3E50] text-2xl font-bold text-center">Регистрация</h2>
+            <div className="flex items-center justify-center w-full h-[70vh]">
+                <div className="w-full max-w-[100%] mx-4">
+                    <form className="bg-transparent">
+                        <div className="flex flex-col items-center space-y-4 mt-8">
+                            <input
+                                type="text"
+                                className="montserrat-text w-[95%] h-[70px] rounded-[20px] bg-[#34495E] text-white placeholder:text-white placeholder:text-xs placeholder:font-bold pl-8"
+                                placeholder="Ваше Имя"
+                            />
 
-            <div className="flex flex-col items-center space-y-4 mt-8">
-                {/* Фото профиля */}
-                <Image
-                    src={avatar}
-                    alt="user avatar"
-                    className="rounded-full"
-                    width={82}
-                    height={82}
-                />
+                            <input
+                                type="text"
+                                className="montserrat-text w-[95%] h-[70px] rounded-[20px] bg-[#34495E] text-white placeholder:text-white placeholder:text-xs placeholder:font-bold pl-8"
+                                placeholder="Ваш ID"
+                            />
+                        </div>
 
-                {/* ФИО и данные */}
-                <div className="text-center">
-                    <p className="text-[#2C3E50] text-base font-bold">
-                        {user.first_name} {user.last_name}
-                    </p>
-                    <p className="text-[#2C3E50] text-base font-bold">
-                        Ваш балл за ЕГЭ:{" "}
-                        <b className="text-[#3498DB] font-bold">256</b>
-                    </p>
-                    <button className="text-[#2C3E50] text-base font-bold mt-2">
-                        Пригласить друга
-                    </button>
+                        <div className="flex flex-col space-y-4 items-center mt-[18px]">
+                            <button
+                                onClick={() => router.push('/profile')}
+                                className="montserrat-text w-[95%] h-[50px] text-white bg-[#3498DB] rounded-[20px] text-xs font-bold"
+                            >
+                                Зарегистрироваться
+                            </button>
+                        </div>
+                    </form>
+                    <div className="flex items-center justify-center mt-[18px]">
+                        <p className="montserrat-text text-[16px] text-[#34495E] font-bold">Есть аккаунт?</p>
+                        <button onClick={() => router.push("/")}
+                                className="montserrat-text text-[16px] text-[#3498DB] font-bold underline ml-[4px]">Войти
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
-    );
+    )
 }
