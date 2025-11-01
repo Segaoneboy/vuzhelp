35fdd6e73eb211abff187912ebb00978c61d3488
SworZzey <i190907@yandex.ru>
2025-11-01 19:13:44 +0700

    поиск ошибки  ngrok

--- src/app/myvuz/page.tsx
+++ src/app/myvuz/page.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-
-const myvuz = () => {
-    return (
-        <div>
-            <button className="pt-[16px] text-[#3498DB] ">Назад</button>
-            <h2 className="pt-[30px] text-[#2C3E50] text-2xl font-bold text-center">Личный кабинет</h2>
-        </div>
-    );
-};
-
-export default myvuz;
+// import React from 'react';
+//
+// const myvuz = () => {
+//     return (
+//         <div>
+//             <button className="pt-[16px] text-[#3498DB] ">Назад</button>
+//             <h2 className="pt-[30px] text-[#2C3E50] text-2xl font-bold text-center">Личный кабинет</h2>
+//         </div>
+//     );
+// };
+//
+// export default myvuz;
