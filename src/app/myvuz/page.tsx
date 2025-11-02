ba5463f83d69c8aad029852dd2c3816cf645aee6
SworZzey <i190907@yandex.ru>
2025-11-02 19:46:15 +0700

    поиск ошибки

--- src/app/myvuz/page.tsx
+++ src/app/myvuz/page.tsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import CardVuz from "@/components/CardVuz";
-import { useRouter } from "next/navigation"
 
 const myvuz = () => {
-    const router = useRouter();
     return (
         <div className="bg-[#ECF0F1] min-h-screen">
             <button
                 className="pt-[16px] text-[#3498DB] font-bold"
-                onClick={() => router.push('/profile')}
             >
                 Назад
             </button>
-            <h2>Мои вузы</h2>
+            <h2 className="pt-[30px] text-[#2C3E50] text-2xl font-bold text-center">Мои вузы</h2>
             <div>
-                {/*<CardVuz number={1} vuzName="МГУ"/>*/}
+                <CardVuz number={1} vuzName="МГУ"/>
             </div>
         </div>
     );
--- src/components/CardVuz.tsx
+++ src/components/CardVuz.tsx
@@ -10,15 +10,16 @@ const CardVuz = (props: CardVuzProps) => {
     const arrowDown = '/arrow.svg'
     const {number, vuzName} = props;
     return (
-        <div>
-            <p>{number}</p>
-            <p>{vuzName}</p><Image
-            src={arrowDown}
-            alt="user avatar"
-            className="rounded-full"
-            width={29}
-            height={29}
-        />
+        <div className="w-95% bg-[#34495E] rounded-2xl mt-5">
+            <p className="font-bold text-[12px] text-[#ffffff]">{number}</p>
+            <p className="font-bold text-[16px] text-[#ffffff]">{vuzName}</p>
+            <Image
+                src={arrowDown}
+                alt="user avatar"
+                className="rounded-full"
+                width={29}
+                height={29}
+            />
         </div>
     )
 }
