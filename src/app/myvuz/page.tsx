23cdfe6bd0318b555daf9aa81d8d17481a5c11b9
SworZzey <i190907@yandex.ru>
2025-11-02 19:23:33 +0700

    компонент карточки вуза без стилей

--- src/app/myvuz/page.tsx
+++ src/app/myvuz/page.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import CardVuz from "@/components/CardVuz";
 
 const myvuz = () => {
     return (
-        <div>
-            <button className="pt-[16px] text-[#3498DB] ">Назад</button>
-            <h2 className="pt-[30px] text-[#2C3E50] text-2xl font-bold text-center">Личный кабинет</h2>
+        <div className="bg-[#ECF0F1] min-h-screen">
+            <button className="pt-[16px] text-[#3498DB] font-bold">Назад</button>
+            <h2 className="pt-[30px] text-[#2C3E50] text-2xl font-bold text-center">Мои вузы</h2>
+            <div>
+                <CardVuz number={1} vuzName="МГУ"/>
+            </div>
         </div>
     );
 };
---
+++ src/components/CardVuz.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Image from "next/image";
+
+interface CardVuzProps {
+    number: number;
+    vuzName: string;
+}
+
+const CardVuz = (props: CardVuzProps) => {
+    const arrowDown = '/arrow.svg'
+    const {number, vuzName} = props;
+    return (
+        <div>
+            <p>{number}</p>
+            <p>{vuzName}</p>
+            <Image
+                src={arrowDown}
+                alt="user avatar"
+                className="rounded-full"
+                width={29}
+                height={29}
+            />
+        </div>
+    )
+}
+
+export default CardVuz;
