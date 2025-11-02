72126dd2558a78e1c5276759a35933a3000fd13f
SworZzey <i190907@yandex.ru>
2025-11-02 19:51:54 +0700

    Стили карточки вуза

--- src/components/CardVuz.tsx
+++ src/components/CardVuz.tsx
@@ -10,7 +10,7 @@ const CardVuz = (props: CardVuzProps) => {
     const arrowDown = '/arrow.svg'
     const {number, vuzName} = props;
     return (
-        <div className="w-[90%] h-[58px] bg-[#34495E] rounded-2xl mt-5 flex items-center justify-between px-5">
+        <div className="w-[90%] h-[58px] bg-[#34495E] rounded-2xl mt-5 flex items-center justify-between px-5 mx-auto">
             <p className="font-bold text-[12px] text-white ml-[20px]">{number}</p>
             <p className="font-bold text-[16px] text-white absolute left-1/2 transform -translate-x-1/2">{vuzName}</p>
             <Image
