0a33ade37c2259ec6fdc751cae9fb2b6a01947f1
SworZzey <i190907@yandex.ru>
2025-12-02 19:42:45 +0700

    Добавление списка направлений

--- src/components/CardNapr.tsx
+++ src/components/CardNapr.tsx
@@ -10,7 +10,7 @@ const CardNapr = (props: CardProps) => {
     const {chanse, name} = props;
 
     return (
-        <div className="w-[90%] h-[40px] bg-[#111856] rounded-2xl mt-3 flex items-center justify-between px-5 mx-auto">
+        <div className="w-[90%] h-[40px] bg-[#111856] rounded-2xl mt-1.5 flex items-center justify-between px-5 mx-auto">
             <p className="font-bold text-[18px] text-white absolute left-1/2 transform -translate-x-1/2">{name}</p>
             <p className="font-bold text-[18px] text-white ml-[20px]">{chanse}</p>
         </div>
