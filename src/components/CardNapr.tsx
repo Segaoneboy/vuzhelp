26942318aaa17bffa352ccda56362e97e0e8c52c
SworZzey <i190907@yandex.ru>
2025-12-02 19:45:22 +0700

    Стили списка направлений

--- src/components/CardNapr.tsx
+++ src/components/CardNapr.tsx
@@ -11,8 +11,8 @@ const CardNapr = (props: CardProps) => {
 
     return (
         <div className="w-[90%] h-[40px] bg-[#111856] rounded-2xl mt-1.5 flex items-center justify-between px-5 mx-auto">
-            <p className="font-bold text-[18px] text-white absolute left-1/2 transform -translate-x-1/2">{name}</p>
             <p className="font-bold text-[18px] text-white ml-[20px]">{chanse}</p>
+            <p className="font-bold text-[18px] text-white absolute left-1/2 transform -translate-x-1/2">{name}</p>
         </div>
     );
 };
