6244b80e573c1776f321a6e3774e97de17dcffd4
SworZzey <i190907@yandex.ru>
2025-12-02 19:52:46 +0700

    Стили списка направлений

--- src/components/CardNapr.tsx
+++ src/components/CardNapr.tsx
@@ -11,7 +11,7 @@ const CardNapr = (props: CardProps) => {
 
     return (
         <div className="w-[90%] h-[40px] bg-[#111856] rounded-2xl mt-1.5 flex items-center px-5 mx-auto">
-            <div className="flex-1 text-center ml-2">
+            <div className="flex-1 text-center ml-10">
                 <p className="font-bold text-[18px] text-white">{name}</p>
             </div>
             <p className="font-bold text-[18px] text-white ml-4">{chanse}</p>
